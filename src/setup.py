0c1af16cf58601ea4903f4cba98970404c2479e9
atvcaptain <openatv@gmail.com>
2026-04-02 20:52:37 +0200

    update ipk name to RemoteControlCode

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 import setup_translate
 
-pkg = 'SystemPlugins.VuRemote'
+pkg = 'SystemPlugins.RemoteControlCode'
 setup(name='enigma2-plugin-systemplugins-remotecontrolcode',
        version='3.0',
        description='Change Remote Control Code',
-       package_dir={pkg: 'VuRemote'},
+       package_dir={pkg: 'RemoteControlCode'},
        packages=[pkg],
        package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass,  # for translation
